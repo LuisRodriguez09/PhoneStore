f224b874c26b52db1813c438611787e3446356be
LuisRodriguez09 <mdautt123ps3@gmail.com>
2024-12-17 23:09:53 -0600

    feat: product

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import Home from "../layout/Home";
 import Cart from "../layout/Cart";
+import Product from "../layout/Product";
 
 export interface Route {
   path: string;
@@ -31,6 +32,14 @@ export const routesList: Route[] = [
     handle: {
       breadcrumb: "Cart",
       title: "Cart",
+    },
+  },
+  {
+    path: "/product/:productId",
+    element: <Product />,
+    handle: {
+      breadcrumb: "Product",
+      title: "Product",
     },
   },
 ];
